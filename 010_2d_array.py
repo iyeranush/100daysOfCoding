95fa274d14df6e111bc852b1f7ac642c3aed8481
Anusha Iyer <anusha@authentise.com>
2018-01-31 14:34:52 -0700

    Fix for supporting negative values
    When all the values in the arrays are neg, having a start as 0 does not
    help,
    Instead we assign the first calculated sum as the start.

--- 010_2d_array.py
+++ 010_2d_array.py
@@ -19,7 +19,6 @@ e f g
 
 import sys
 
-
 arr = []
 for arr_i in range(6):
     arr_temp = input().strip().split(' ')
@@ -27,12 +26,12 @@ for arr_i in range(6):
     arr.append(arr_temp)
 
 max_depth = 6 - 2
-max_sum = 0
+max_sum = None
 for i in range(max_depth):
     for j in range(max_depth):
         hourglass_arr = [arr[i][j], arr[i][j+1], arr[i][j+2], arr[i+1][j+1], arr[i+2][j], arr[i+2][j+1], arr[i+2][j+2]]
         hourglass_sum = sum(hourglass_arr)
-        if max_sum < hourglass_sum:
+        if max_sum is None or (max_sum and max_sum < hourglass_sum):
             max_sum = hourglass_sum
 
 print(max_sum)
