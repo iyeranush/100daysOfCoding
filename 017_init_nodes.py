0e3df2213ae454fc09d4da852548b4ffa582a308
Anusha Iyer <anusha@authentise.com>
2018-02-04 17:37:45 -0700

    Have code initialization for nodes
    Contains Tree node and linkedlist node

---
+++ 017_init_nodes.py
@@ -0,0 +1,10 @@
+class TreeNode(Object):
+  def __init__(self,x):
+    self.val = x
+    self.left = None
+    self.right = None
+
+class ListNode(Object):
+  def __init__(self,x):
+    self.val = x
+    self.next = None
