9a662c51f6932584f8ad741bdc659afe74bdadb1
Anusha Iyer <anusha@authentise.com>
2018-03-14 23:55:18 -0600

    Fix some indentation

--- 002_multiples_of_3_and_5.py
+++ 002_multiples_of_3_and_5.py
@@ -12,14 +12,14 @@ Find the sum of all the multiples of 3 or 5 below given user input integer.
 def sum_up_3_or_5_multiples(upper_range):
     sum = 0
     for number in range(upper_range):
-	if number % 3 == 0 or number % 5 == 0:
-	    sum += number
+        if number % 3 == 0 or number % 5 == 0:
+            sum += number
     return sum
 
 def main():
     upper_range = int(input("Please enter a number to find sum of all multiples of 3/5 below that."))
     sum = sum_up_3_or_5_multiples(upper_range)
-	print("Sum of all the multiples of 3/5 below ", upper_range, " is: ", sum)
+    print("Sum of all the multiples of 3/5 below ", upper_range, " is: ", sum)
 
 if __name__ == "__main__":
     main()
